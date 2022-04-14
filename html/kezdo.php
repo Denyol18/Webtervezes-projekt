27df96871d146f0dac12f68945289cb557a2d245
Denyol18 <101126566+Denyol18@users.noreply.github.com>
2022-04-14 21:40:14 +0200

    Update kezdo.php

--- html/kezdo.php
+++ html/kezdo.php
@@ -1,3 +1,66 @@
+<?php
+    include_once "../classes/User.php";
+    include_once "../common/methods.php";
+
+    $users = loadData("../data/users.txt");
+
+    $errors = [];
+
+    if(isset($_POST["signup"])) {
+        $username = $_POST["username"];
+        $age = $_POST["age"];
+        $sex = "egyéb";
+        $password = $_POST["psswd"];
+        $checker = $_POST["psswd1"];
+        $email = $_POST["email"];
+
+        if (isset($_POST["sex"])) {
+            $sex = $_POST["sex"];
+        }
+
+        if (trim($username) === "" || trim($age) === "" || trim($password) === "" || trim($checker) === ""
+            || trim($email) === "") {
+            $errors[] = "Minden kötelezően kitöltendő mezőt ki kell tölteni!";
+        }
+
+        foreach ($users as $user) {
+            if ($user->getUsername() === $username) {
+                $errors[] = "A felhasználónév már foglalt!";
+            }
+        }
+
+        if (strlen($password) < 4) {
+            $errors[] = "A jelszónak legalább 4 karakter hosszúnak kell lennie!";
+        }
+
+        if (!preg_match("/[A-Za-z]/", $password) || !preg_match("/[0-9]/", $password)) {
+            $errors[] = "A jelszónak tartalmaznia kell betűt és számjegyet is!";
+        }
+
+        if (!preg_match("/[0-9a-z.-]+@([0-9a-z-]+\.)+[a-z]{2,4}/", $email)) {
+            $errors[] = "A megadott e-mail cím formátuma nem megfelelő!";
+        }
+
+        if ($password !== $checker) {
+            $errors[] = "A két megadott jelszó nem egyezik!";
+        }
+
+        foreach ($users as $user) {
+            if ($user->getEmail() === $email) {
+                $errors[] = "Az e-mail cím már foglalt!";
+            }
+        }
+
+        if (count($errors) === 0) {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $user = new User($username, $age, $sex, $password, $email);
+            $users[] = $user;
+            saveData("../data/users.txt", $users);
+        }
+
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -30,6 +93,18 @@
     <main class="fadeIn">
 
         <section id="udvozlo">
+
+            <?php
+            if (count($errors) > 0) {
+                echo "<div class='errors broder'>";
+
+                foreach ($errors as $error) {
+                    echo "<p>" . $error . "</p>";
+                }
+
+                echo "</div>";
+            }
+            ?>
 
             <h2>Üdvözlő</h2>
 
@@ -79,7 +154,7 @@
 
             <h2>Vélemény megosztó</h2>
 
-            <form autocomplete="off">
+            <form action="kezdo.php" method="POST" autocomplete="off">
 
                 <fieldset>
 
@@ -93,19 +168,19 @@
                     <br/><br/>
 
                     Nem: <label for="option1">Férfi:</label>
-                    <input type="radio" id="option1" name="sex" checked/>
+                    <input type="radio" id="option1" name="sex"/>
 
                     <label for="option2">Nő:</label>
                     <input type="radio" id="option2" name="sex"/>
 
                     <label for="option3">Egyéb:</label>
-                    <input type="radio" id="option3" name="sex"/>
+                    <input type="radio" id="option3" name="sex" checked/>
                     <br/><br/>
 
                     <label>Jelszó:<input type="password" name="psswd" required></label>
                     <br/><br/>
 
-                    <label>Jelszó ismét:<input type="password" name="psswd" required></label>
+                    <label>Jelszó ismét:<input type="password" name="psswd1" required></label>
                     <br/><br/>
 
                     <label>E-mail:<input type="email" name="email" placeholder="valaki@valami.com" required></label>
@@ -133,16 +208,16 @@
 
                     Mik tetszettek benne/bennük?
                     <label for="pos"></label> <br/><br/>
-                    <textarea id="pos" name="positives" rows="20" cols="60" maxlength="1000" required></textarea>
+                    <textarea id="pos" name="positives" rows="20" cols="60" maxlength="1000"></textarea>
                     <br/><br/>
 
                     Mik NEM tetszettek benne/bennük?
                     <label for="neg"></label> <br/><br/>
-                    <textarea id="neg" name="negatives" rows="20" cols="60" maxlength="1000" required></textarea>
+                    <textarea id="neg" name="negatives" rows="20" cols="60" maxlength="1000"></textarea>
                     <br/><br/>
 
-                    <input type="reset" name="rst" value="Visszaállítás">
-                    <input type="submit" name="sbmt" value="Küldés">
+                    <input type="reset" name="reset" value="Visszaállítás">
+                    <input type="submit" name="signup" value="Küldés">
 
                 </fieldset>
             </form>
@@ -151,9 +226,9 @@
 
     </main>
 
-    <footer>
-        <h3 class="kozepre">Készítette: Sprok Dániel és Megyesi Ferenc</h3>
-    </footer>
+    <?php
+    include_once "../common/footer.php";
+    ?>
 
 </body>
 </html>

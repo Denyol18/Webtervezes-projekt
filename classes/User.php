4c19f8a64cef5e24a4aa6d08c03435ee897ad40f
Denyol18 <101126566+Denyol18@users.noreply.github.com>
2022-04-14 21:47:40 +0200

    Add files via upload

---
+++ classes/User.php
@@ -0,0 +1,75 @@
+<?php
+
+class User
+{
+    private string $username;
+    private int $age;
+    private string $sex;
+    private string $password;
+    private string $email;
+
+    public function __construct(string $username, int $age, string $sex="egyéb", string  $password, string $email)
+    {
+        $this->username = $username;
+        $this->age = $age;
+        $this->sex = $sex;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    public function getSex(): string
+    {
+        return $this->sex;
+    }
+
+    public function setSex(string $sex): void
+    {
+        $this->sex = $sex;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+    public function __toString(): string
+    {
+        return $this->username . $this->age ." éves, neme: ". $this->sex . ", e-mail cím: " . $this->email;
+    }
+
+}

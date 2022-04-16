18474e9f38ac791ee814c1d4eed2cfdcc1b44c42
Denyol18 <101126566+Denyol18@users.noreply.github.com>
2022-04-16 21:33:29 +0200

    Update ferigame1.php

--- pages/ferigame1.php
+++ pages/ferigame1.php
@@ -172,26 +172,10 @@
 
     </main>
 
-    <nav id="lent">
-
-        <ul>
-            <li>
-                <a href="kezdo.php">Kezdőlap</a>
-            </li>
-            <li>
-                <a href="danigame1.php">Dani kedvencei part 1</a>
-            </li>
-            <li>
-                <a href="danigame2.php">Dani kedvencei part 2</a>
-            </li>
-            <li>
-                <a id="active" href="ferigame1.php">Feri kedvencei part 1</a>
-            </li>
-            <li>
-                <a href="ferigame2.php">Feri kedvencei part 2</a>
-            </li>
-        </ul>
-    </nav>
+    <?php
+    include_once "../common/methods.php";
+    generateNavigation("ferigame1");
+    ?>
 
     <?php
     include_once "../common/footer.php";
